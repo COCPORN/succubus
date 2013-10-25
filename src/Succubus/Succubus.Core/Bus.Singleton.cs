4b50304cf152a9320385a54ce10b4be8b5d664b8
Lars Thomas Denstad <cocporn@gmail.com>
2013-10-25 10:21:21 +0200

    Initial checkin.

---
+++ src/Succubus/Succubus.Core/Bus.Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Succubus.Core
+{
+    public partial class Bus
+    {
+        private static volatile Bus instance;
+        private static object syncRoot = new Object();
+
+        public static Bus Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Bus();
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
