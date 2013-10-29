0ae40fe81c3e76cfb4e3a0db76e9a44d17020627
COCPORN <cocporn@gmail.com>
2013-10-29 17:54:17 +0100

    Initial checkin of tests

---
+++ src/Succubus/Tests/Succubus.Tests.Collections/TimedoutDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Succubus.Collections.Test
+{
+    [TestFixture]
+    public class TimedOutDictionaryTest
+    {
+
+        [Test]
+        public void Test2()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Testing R# testrunner");
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
