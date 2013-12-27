96ca05bd8cef9ad083b99d7ba373c27d28f26205
COCPORN <cocporn@gmail.com>
2013-12-27 23:49:27 +0100

    Refactored Core to not be reliant on Hosting. Eventstore is new and INCOMPLETE.

---
+++ src/Succubus/Succubus.EventStore/HostConfigurator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Common.Utils;
+using Newtonsoft.Json;
+using Succubus.Interfaces;
+
+namespace Succubus.Stores.EventStore
+{
+    public static class HostConfigurator
+    {
+
+        private static IEventStoreConnection connection;
+        public static void SetEventStoreConnection(this IMessageHost host, IEventStoreConnection connection)
+        {
+            HostConfigurator.connection = connection;
+        }
+
+        public static void EnableEventStore(this IMessageHost messagehost)
+        {           
+            messagehost.ProcessedMessage += MessagehostOnProcessedMessage;
+        }
+
+        private static void MessagehostOnProcessedMessage(object sender, ProcessedMessageEventArgs eventArgs)
+        {
+            var type = eventArgs.Message.GetType();
+            var classAttributes = type.GetCustomAttributes(true);
+
+            string stream = null;
+          
+            foreach (var attribute in classAttributes)
+            {
+                var store = attribute as StoreAttribute;
+                if (store != null)
+                {
+                    stream = store.Stream;
+                }
+            }
+
+            if (stream != null)
+            {
+                //connection.AppendToStreamAsync(stream, ExpectedVersion.Any, 
+                //    new EventData(Guid.NewGuid(), type.ToString(), true, new object().ToJson(), eventArgs.Message.ToJson())
+                //    );
+            }
+        }
+
+      
+    }
+}
---
+++ src/Succubus/Succubus.EventStore/StoreAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Succubus.Stores.EventStore
+{
+    /// <summary>
+    /// Indicate that this message should be put in the EventStore
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class StoreAttribute : Attribute
+    {
+        public string Stream { get; set; }
+
+        public StoreAttribute() : this("Succubus")
+        {
+           
+        }
+
+        public StoreAttribute(string stream)
+        {
+            Stream = stream;
+        }
+
+    }
+}
