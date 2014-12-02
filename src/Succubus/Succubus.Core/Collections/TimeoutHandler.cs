f2145395c5b2d73e62995284c8ed024528713acc
Lars Thomas Denstad <cocporn@gmail.com>
2014-12-02 03:59:31 +0100

    Cleanup and commenting of timeout handler

--- src/Succubus/Succubus.Core/Collections/TimeoutHandler.cs
+++ src/Succubus/Succubus.Core/Collections/TimeoutHandler.cs
@@ -10,9 +10,14 @@ namespace Succubus.Collections
         public DateTime Now { get { return DateTime.Now; } }
     }
 
+    /// <summary>
+    /// A general purpose timeout handler
+    /// </summary>
+    /// <typeparam name="TKey">the key type</typeparam>
+    /// <typeparam name="TValue">the value type</typeparam>
     public class TimeoutHandler<TKey, TValue> where TValue : IExpiring<TKey>
     {
-        private readonly SortedDictionary<Int64, TValue> sortedTimeoutSynchronizationContexts
+        private readonly SortedDictionary<Int64, TValue> sortedItems
             = new SortedDictionary<Int64, TValue>();
 
         private readonly AutoResetEvent timeoutResetEvent = new AutoResetEvent(false);
@@ -25,66 +30,100 @@ namespace Succubus.Collections
             timeoutThread.Start();
         }
 
+        // This thread runs through the sorted dictionary
+        // to find expired entries. As it is sorted on ticks, it can start
+        // in the beginning, and knows when to wait.
+
+        // The amount of time to wait is calculated from the last remaining
+        // unexpired entry and current time
         void TimeoutThread()
         {
+            // Just an arbitrary value to start with
             int waitmilliseconds = 100;
             while (true)
-            {
+            {                
                 if (waitmilliseconds != 0) timeoutResetEvent.WaitOne(waitmilliseconds);
+
+                // Because you can't change a set of data while iterating over it, copy out values
                 var removeKeys = new List<Int64>();
-                lock (sortedTimeoutSynchronizationContexts)
+
+
+                lock (sortedItems)
                 {
+
+                    // Assume we want to wait indefinitely. This is what will happen with an empty
+                    // sequence
                     waitmilliseconds = -1;
-                    foreach (var entry in sortedTimeoutSynchronizationContexts)
+                    foreach (var entry in sortedItems)
                     {
+                        // Check to see whether or not the entry has expired. This should
+                        // be changed to using INow, as described over
                         var timespan = TimeSpan.FromTicks(entry.Key - DateTime.Now.Ticks);
+
                         var comparison = timespan.CompareTo(new TimeSpan(0, 0, 0));
+
+                        // The head of the list isn't yet expired, so beak out of the loop
                         if (comparison == 1)
                         {
+                            // How long am I supposed to wait now?
                             waitmilliseconds = timespan.Milliseconds;
                             break;
                         }
                         entry.Value.TimedOut = true;
                         entry.Value.OnTimeout();
-
+                        
                         if (entry.Value.TimeoutHandler != null)
                         {
                             entry.Value.TimeoutHandler();
                         }
-
-                        
-                   
+ 
+                        // Add key to set of keys to be removed from the sorted items
                         removeKeys.Add(entry.Key);
                         timeoutContexts.Remove(entry.Value.Id);
                     }
 
                     foreach (var key in removeKeys)
                     {
-                        sortedTimeoutSynchronizationContexts.Remove(key);
+                        sortedItems.Remove(key);
                     }
+                // End of lock on sorted items
+                
                 }
             }
         }
 
+        /// <summary>
+        /// A debug helper to visualize the current timeout table
+        /// </summary>
         public void DumpTimeoutTable()
         {
-            lock (sortedTimeoutSynchronizationContexts)
+            lock (sortedItems)
             {
-                foreach (var entry in sortedTimeoutSynchronizationContexts)
+                foreach (var entry in sortedItems)
                 {
                     Console.WriteLine("In {0}", TimeSpan.FromTicks((entry.Key - DateTime.Now.Ticks)));
                 }
             }
         }
 
+        /// <summary>
+        /// The count of how many items are waiting to time out
+        /// </summary>
+        /// <returns></returns>
         public int NumberOfItemsForTimeout()
         {
-            lock (sortedTimeoutSynchronizationContexts)
+            lock (sortedItems)
             {
-                return sortedTimeoutSynchronizationContexts.Count;
+                return sortedItems.Count;
             }
         }
 
+        /// <summary>
+        /// Set an entity to time out
+        /// </summary>
+        /// <param name="value">the entity</param>
+        /// <param name="milliseconds">the timeout</param>
+        /// <returns></returns>
         public Int64 Timeout(TValue value, int milliseconds)
         {
             var timespan = TimeSpan.FromMilliseconds(milliseconds);
@@ -92,14 +131,21 @@ namespace Succubus.Collections
 
             Int64 timeoutTick = timeoutDateTime.Ticks;
 
-            lock (sortedTimeoutSynchronizationContexts)
+            lock (sortedItems)
             {
-
-                while (sortedTimeoutSynchronizationContexts.ContainsKey((timeoutTick)))
+                // Make sure the sorted dictionary doesn't already contain an entry
+                // at this tick
+                while (sortedItems.ContainsKey((timeoutTick)))
                 {
                     timeoutTick++;
                 }
-                sortedTimeoutSynchronizationContexts.Add(timeoutTick, value);
+
+                // Add the value to the sorted list the thread is working on expiring
+                sortedItems.Add(timeoutTick, value);
+
+                // Because a single item can have multiple timeouts assigned to it,
+                // we need to keep track of all of them, as there is an option
+                // of removing the timeout
                 List<Int64> keys;
                 if (timeoutContexts.TryGetValue(value.Id, out keys) == false)
                 {
@@ -112,67 +158,34 @@ namespace Succubus.Collections
                     keys.Add(timeoutTick);
                 }
             }
+
+            // Because the timeout thread could be blocking indefinitely waiting
+            // on -1, adding a new entry to the sorted list forces us to signal 
+            // it to wake up. This will allow the timeout thread to recalculate
+            // its sleep time
             timeoutResetEvent.Set();
             return timeoutTick;
         }
 
-        public void RemoveTimeout(TKey correlationId)
+        /// <summary>
+        /// Remove timeout for a given entry, this will stop timeout handlers from getting called
+        /// </summary>
+        /// <param name="id">the entry</param>
+        public void RemoveTimeout(TKey id)
         {
-            lock (sortedTimeoutSynchronizationContexts)
+            lock (sortedItems)
             {
                 List<Int64> keys;
-                if (timeoutContexts.TryGetValue(correlationId, out keys))
+                if (timeoutContexts.TryGetValue(id, out keys))
                 {
                     foreach (var key in keys)
                     {
-                        sortedTimeoutSynchronizationContexts.Remove(key);
+                        sortedItems.Remove(key);
                     }
-                    timeoutContexts.Remove(correlationId);
+                    timeoutContexts.Remove(id);
                 }
             }
         }
 
-        public IEnumerable<TValue> GetValues(TKey correlationId)
-        {
-            List<TValue> values = new List<TValue>();
-            lock (sortedTimeoutSynchronizationContexts)
-            {
-               List<Int64> keys;
-                if (timeoutContexts.TryGetValue(correlationId, out keys))
-                {
-                    foreach (var key in keys)
-                    {
-                        values.Add(sortedTimeoutSynchronizationContexts[key]);
-                    }
-                }
-              
-            }
-            foreach (var value in values)
-            {
-                yield return value;
-            }
-        }
-
-        public IEnumerable<TValue> GetAndRemoveValues(TKey correlationId)
-        {
-            List<TValue> values = new List<TValue>();
-            lock (sortedTimeoutSynchronizationContexts)
-            {
-                List<Int64> keys;
-                if (timeoutContexts.TryGetValue(correlationId, out keys))
-                {
-                    foreach (var key in keys)
-                    {
-                        values.Add(sortedTimeoutSynchronizationContexts[key]);
-                    }
-                }
-
-            }
-            RemoveTimeout(correlationId);
-            foreach (var value in values)
-            {
-                yield return value;
-            }
-        }
     }
 }
